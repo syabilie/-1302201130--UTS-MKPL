cd54de03c8c5b597ff585bd055dc81452aa74c88
ngangong <selsa844@gmail.com>
2023-04-16 00:14:37 +0700

    mengganti objek kompleks

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,42 +3,50 @@ package lib;
 public class TaxFunction {
 
 	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
+	private static final int MAX_CHILDREN = 3;
+	private static final double TAX_RATE = 0.05;
 	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+	public static int calculateTax(double monthlySalary, double otherMonthlyIncome, int numberOfMonthsWorking, double deductible, boolean isMarried, int numberOfChildren) {
 		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
+		if (numberOfMonthsWorking > 12) {
 			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
 			return 0;
-		}else {
-			return tax;
 		}
-			 
+		
+		// Hitung penghasilan tidak kena pajak berdasarkan status perkawinan dan jumlah anak
+		double nonTaxableIncome = calculateNonTaxableIncome(isMarried, numberOfChildren);
+		
+		// Menghitung penghasilan kena pajak
+		double taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome, numberOfMonthsWorking, deductible, nonTaxableIncome);
+		
+		// Calculate tax amount
+		int taxAmount = calculateTaxAmount(taxableIncome);
+		
+		return taxAmount;
 	}
+	
+	private static double calculateNonTaxableIncome(boolean isMarried, int numberOfChildren) {
+		double nonTaxableIncome = 0;
+		if (isMarried) {
+			nonTaxableIncome += 2_000_000;
+		} else {
+			nonTaxableIncome += 1_000_000;
+		}
+		nonTaxableIncome += numberOfChildren * 500_000;
+		nonTaxableIncome *= 12;
+		return nonTaxableIncome;
+	}
+	
+	private static double calculateTaxableIncome(double monthlySalary, double otherMonthlyIncome, int numberOfMonthsWorking, double deductible, double nonTaxableIncome) {
+		double totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthsWorking;
+		totalIncome -= deductible;
+		totalIncome -= nonTaxableIncome;
+		return totalIncome;
+	}
+	
+	private static int calculateTaxAmount(double taxableIncome) {
+		return (int) Math.ceil(taxableIncome * TAX_RATE);
+	}
+	
 	
 }
